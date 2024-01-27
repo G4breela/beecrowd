69ec5f166817d91e8eb69a8fb400941c6ea3ddc4
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-27 20:36:23 -0300

    Update 1004.js

---
+++ iniciante/1004.js
@@ -0,0 +1,9 @@
+// Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.  
+
+let [a, b] = [lines[0], lines[1]];
+// para testar atribua o valor. Ex: [3 , 9]
+
+let PROD = a*b;
+
+console.log(`PROD = ${PROD}`);
+
