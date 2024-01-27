1c81caf36a1655505e925fa9ba14bdb95aee16b0
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-27 20:28:44 -0300

    Update 1002.js

---
+++ iniciante/1002.js
@@ -0,0 +1,12 @@
+// A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+
+// - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+let raio = parseFloat(lines.shift());
+// para testar adicione o valor. ex: 100.64;
+
+let n = 3.14159;
+
+let area = n*(Math.pow(raio, 2));
+
+console.log(`A=${area.toFixed(4)}`)
