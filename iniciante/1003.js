ac34fae4053d7759113592cef7adc7ccac6e0759
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-27 20:31:37 -0300

    Update 1003.js

---
+++ iniciante/1003.js
@@ -0,0 +1,8 @@
+// Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+
+let SOMA = A + B;
+
+console.log(`SOMA = ${SOMA}`);
