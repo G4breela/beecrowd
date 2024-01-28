141c667bc5716c1933bb1cb3f7205c2a590d1b37
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-28 15:25:03 -0300

    Update 1018.js

--- iniciante/1018.js
+++ iniciante/1018.js
@@ -3,6 +3,7 @@
 let valor = parseInt(lines.shift());
 // para testar atribua um valor. Ex: 11257
 
+console.log(valor);
 console.log(`${Math.floor(valor/100)} nota(s) de R$ 100,00`);
 valor %= 100;
 console.log(`${Math.floor(valor/50)} nota(s) de R$ 50,00`);
@@ -15,4 +16,4 @@ console.log(`${Math.floor(valor/5)} nota(s) de R$ 5,00`);
 valor %= 5;
 console.log(`${Math.floor(valor/2)} nota(s) de R$ 2,00`);
 valor %= 2;
-console.log(`${Math.floor(valor)} nota(s) de R$ 1,00`);
+console.log(`${valor} nota(s) de R$ 1,00`);
